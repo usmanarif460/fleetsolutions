2d9a37a39ccb0ef2cbd7c76729d1d35e410dc73c
usmanarif460 <usmanarif460@gmail.com>
2024-05-29 11:30:29 +0500

    feat:setup directories

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="bg-green-500 ">Star Devs here</h1>
+    <NuxtPage />
   </div>
 </template>
--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1 class="bg-green-500 ">Star Devs here</h1>
+        <h1 class="bg-green-500 ">Full Stack Devs here</h1>
     </div>
 </template>
 
