db83552b297b5d5238433675ebaf8943d538f898
usmanarif460 <usmanarif460@gmail.com>
2024-06-02 02:31:23 +0500

    fix:install fomrkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    "@nuxtjs/tailwindcss",
+    '@formkit/nuxt'
+  ],
+  formkit: {
+    // Experimental support for auto loading (see note):
+    autoImport: true
+  }
 })
--- pages/test.vue
+++ pages/test.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1 class="bg-green-500 ">Full Stack Devs here</h1>
+        <FormKit type="text" label="Usman Arif" placeholder="Enter your name" />
     </div>
 </template>
 
