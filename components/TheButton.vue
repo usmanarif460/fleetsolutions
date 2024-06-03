a9b1eb325aeee2c5d5b4506848a6e933afb1b72f
sheraz4196 <sheraztoor321@gmail.com>
2024-06-04 00:40:06 +0500

    fix: transition on button hover

--- components/TheButton.vue
+++ components/TheButton.vue
@@ -1,19 +1,21 @@
 <template>
-    <button :type="buttonType"
-        class="bg-primary text-white font-medium rounded-full px-6 py-3 text-lg disabled:bg-gray-100 disabled:cursor-not-allowed hover:bg-purple">
-        {{ buttonText
-        }}</button>
+  <button
+    :type="buttonType"
+    class="bg-primary text-white font-medium rounded-full px-6 py-3 text-lg disabled:bg-gray-100 disabled:cursor-not-allowed transition-all hover:bg-purple"
+  >
+    {{ buttonText }}
+  </button>
 </template>
 
 <script setup>
 defineProps({
-    buttonText: {
-        type: String,
-        default: ''
-    },
-    buttonType: {
-        type: String,
-        default: 'button'
-    }
-})
+  buttonText: {
+    type: String,
+    default: "",
+  },
+  buttonType: {
+    type: String,
+    default: "button",
+  },
+});
 </script>
