03918d79f4bfe7f452a205fe89b565c30bec9bff
sheraz4196 <sheraztoor321@gmail.com>
2024-06-04 20:21:53 +0500

    feat: generic future card

---
+++ components/TheFeatureCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="flex items-center justify-center px-6 py-8 m-4 bg-white hover:bg-purple rounded-lg shadow-lg"
+  >
+    <div>
+      <figure class="w-28 mb-5">
+        <img
+          :width="imgWidth"
+          :height="imgHeight"
+          :src="imgUrl"
+          alt="Feature Photo"
+        />
+      </figure>
+      <div class="flex items-center flex-col gap-4">
+        <h4>
+          {{ heading }}
+        </h4>
+        <p>
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  imgUrl: {
+    type: String,
+    default: "",
+  },
+  imgWidth: {
+    type: Number,
+    default: 50,
+  },
+  imgHeight: {
+    type: Number,
+    default: 50,
+  },
+  heading: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
   <TheHeader />
   <TheHugeSecretSection />
   <TheExample />
+  <TheFeatureCard
+    img-url="/images/crown-left.png"
+    img-height="50"
+    img-width="50"
+    heading="mobile optimized"
+    description="With over 50% of internet traffic coming from cell phones, we make sure your site is fully mobile optimized."
+  />
 </template>
 
 <script setup></script>
