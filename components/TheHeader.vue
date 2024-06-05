b5b66eba28f2fbd8184563efea625ca799a89426
sheraz4196 <sheraztoor321@gmail.com>
2024-06-05 17:50:23 +0500

    fix: header crown descriptions

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,32 +1,33 @@
 <template>
-    <div class="bg-gradient-to-r from-indigo-100 to-violet-400 h-[70vh] w-full object-cover">
-        <div class="max-w-7xl mx-auto w-full flex flex-col items-center justify-center gap-5">
-            <div class="flex w-full">
-                <TheLogo />
-                <div class="flex gap-5 items-center justify-center w-6/12">
-                    <img src="/images/crown-left.png" alt="">
-                    <p class="text-base text-white">Working Since 2013 on several platforms <br> Making <span
-                            class="underline text-primary">Clients</span>
-                        happier
-                        morre
-                        than ever</p>
-                    <img src="/images/crown-right.png" alt="">
-
-                </div>
-            </div>
-
-            <h1 class="w-10/12 text-center text-white text-[3rem] uppercase font-bold">Design Your website, promote it,
-                develop it
-                and
-                grow
-                your <span class="underline text-primary">business</span>
-                with us.
-            </h1>
-            <TheButton button-text="Apply in 2 minutes" />
+  <div
+    class="bg-gradient-to-r from-indigo-100 to-violet-400 h-screen w-full object-cover"
+  >
+    <div
+      class="max-w-7xl mx-auto w-full flex flex-col items-center justify-center gap-5"
+    >
+      <TheLogo />
+      <div class="flex w-full items-center justify-center">
+        <div class="flex gap-5 items-center justify-center w-6/12">
+          <img src="/images/crown-left.png" alt="" />
+          <p class="text-base text-white">
+            Working Since 2013 on several platforms <br />
+            Making <span class="underline text-primary">Clients</span> happier
+            morre than ever
+          </p>
+          <img src="/images/crown-right.png" alt="" />
         </div>
+      </div>
+
+      <h1
+        class="w-10/12 text-center text-white text-[3rem] uppercase font-bold"
+      >
+        Design Your website, promote it, develop it and grow your
+        <span class="underline text-primary">business</span>
+        with us.
+      </h1>
+      <TheButton button-text="Apply in 2 minutes" />
     </div>
+  </div>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
