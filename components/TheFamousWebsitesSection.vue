24e6f0b084fcc44aa356cd93de0e03a43ef5b0ed
sheraz4196 <sheraztoor321@gmail.com>
2024-06-12 02:16:25 +0500

    feat: added real projects with pretty design

--- components/TheFamousWebsitesSection.vue
+++ components/TheFamousWebsitesSection.vue
@@ -4,23 +4,38 @@
       <h3 class="uppercase font-bold lg:text-5xl pb-9 text-center">
         10,000+ websites built
       </h3>
-      <div class="flex items-center justify-between w-full gap-4">
-        <a href="#">
-          <span class="block h-20 w-36 bg-violet-900"></span>
-        </a>
-        <a href="#">
-          <span class="block h-20 w-36 bg-yellow-900"></span>
-        </a>
-        <a href="#">
-          <span class="block h-20 w-36 bg-blue-900"></span>
-        </a>
-        <a href="#">
-          <span class="block h-20 w-36 bg-orange-700"></span>
-        </a>
-        <a href="#">
-          <span class="block h-20 w-36 bg-stone-700"></span>
-        </a>
-      </div>
+      <ol class="flex items-center justify-between w-full gap-12">
+        <li
+          v-for="project in Projects"
+          :key="project.demoLink"
+          class="h-48 px-12 flex items-center justify-center bg-primary hover:bg-primary/80 transition-all cursor-pointer rounded-lg shadow-md"
+        >
+          <a :href="project.demoLink" target="blank">
+            <img
+              :src="project.logo"
+              alt="Project Logo"
+              width="150"
+              height="150"
+            />
+          </a>
+        </li>
+      </ol>
     </div>
   </section>
 </template>
+<script setup>
+const Projects = [
+  {
+    demoLink: "https://park-place.vercel.app/",
+    logo: "https://park-place.vercel.app/assets/logo-dark.svg",
+  },
+  {
+    demoLink: "https://better-merchant.vercel.app/",
+    logo: "https://better-merchant.vercel.app/images/logo.png",
+  },
+  {
+    demoLink: "https://hacking-hr.vercel.app",
+    logo: "https://hacking-hr.vercel.app/assets/logo.svg",
+  },
+];
+</script>
