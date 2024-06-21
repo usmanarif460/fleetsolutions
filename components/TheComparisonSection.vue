b9e2b8d827828561962fb02233e89dd4587dabf0
sheraz4196 <sheraztoor321@gmail.com>
2024-06-21 23:33:27 +0500

    fix: minor ui bug

--- components/TheComparisonSection.vue
+++ components/TheComparisonSection.vue
@@ -2,24 +2,34 @@
   <section class="pt-12 py-24 grid items-center justify-center">
     <div class="max-w-7xl grid lg:grid-cols-3 gap-16 lg:gap-32">
       <div>
-        <p class="md:text-2xl text-lg font-medium"># of free websites we've built</p>
+        <p class="md:text-xl text-lg font-medium">
+          # of free websites we've built
+        </p>
         <div class="flex items-center gap-3">
           <h4 class="md:text-7xl text-xl font-bold">10124</h4>
-          <span class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3" />
+          <span
+            class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3"
+          />
         </div>
       </div>
       <div>
-        <p class="md:text-2xl text-lg font-medium">average cost a web agency charges</p>
+        <p class="md:text-xl text-lg font-medium">
+          average cost a web agency charges
+        </p>
         <div class="flex items-center gap-3">
           <h4 class="md:text-7xl text-2xl font-bold">$3698</h4>
-          <span class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3" />
+          <span
+            class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3"
+          />
         </div>
       </div>
       <div>
-        <p class="md:text-2xl text-lg font-medium">amount we charge</p>
+        <p class="md:text-xl text-lg font-medium">amount we charge</p>
         <div class="flex items-center gap-3">
           <h4 class="md:text-7xl text-xl font-bold">0</h4>
-          <span class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3" />
+          <span
+            class="inline-block h-2 flex-1 bg-purple rounded-lg self-start mt-3"
+          />
         </div>
       </div>
     </div>
