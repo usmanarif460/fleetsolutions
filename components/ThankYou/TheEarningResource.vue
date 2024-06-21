c6239aea5da0f6f0b45a41b2e0d3a68bc4406716
sheraz4196 <sheraztoor321@gmail.com>
2024-06-22 00:38:28 +0500

    fix n refactor: used semantic tags and centred text

--- components/ThankYou/TheEarningResource.vue
+++ components/ThankYou/TheEarningResource.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="py-10">
-    <div>
-      <p>
-        *While we don’t earn money in offering free websites, a percentage of
-        our clients come back to us for paid projects further down the line
-        (which is why we offer an affiliate program). See more about
-        <a href="#" class="underline underline-offset-2 text-blue-900 font-bold"
-          >why we offer free websites</a
-        >, and our affiliate program’s
-        <a href="#" class="underline underline-offset-2 text-blue-900 font-bold"
-          >Terms and Conditions.</a
-        >
-      </p>
-    </div>
-  </div>
+  <article class="py-10 text-center">
+    <p>
+      *While we don’t earn money in offering free websites, a percentage of our
+      clients come back to us for paid projects further down the line (which is
+      why we offer an affiliate program). See more about
+      <a href="#" class="underline underline-offset-2 text-blue-900 font-bold"
+        >why we offer free websites</a
+      >, and our affiliate program’s
+      <a href="#" class="underline underline-offset-2 text-blue-900 font-bold"
+        >Terms and Conditions.</a
+      >
+    </p>
+  </article>
 </template>
 <script setup></script>
