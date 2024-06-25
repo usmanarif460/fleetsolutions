429cbc773e44ef3e2ebe0c056e36e77a2122bf58
sheraz4196 <sheraztoor321@gmail.com>
2024-06-26 00:57:01 +0500

    fix: minor issue with props

--- components/TheLogo.vue
+++ components/TheLogo.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <img class="w-24 h-24 object-cover" :class="className" src="/images/2.png" alt="" />
+    <img
+      class="w-24 h-24 object-cover"
+      :class="className"
+      src="/images/2.png"
+      alt=""
+    />
   </div>
 </template>
 
 <script setup>
-defineProps: ({
+defineProps({
   className: {
     type: String,
     default: "",
-  }
+  },
 });
 </script>
